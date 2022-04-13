9b2ff336e785b6d71c62ef3d61ab91162e9643f0
Maxim-85 <k_maks.85@mail.ru>
2022-04-13 23:05:26 +0500

    feat: add prompts in the start script.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 'use strict';
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+const filmOne = prompt('Один из последних просмотренных фильмов', ''),
+      gradeOne = +prompt('Какую оценку поставите?', ''),
+
+      filmTwo = prompt('Один из последних просмотренных фильмов', ''),
+      gradeTwo = +prompt('Какую оценку поставите?', '');
+
+personalMovieDB.movies[filmOne] = gradeOne;
+personalMovieDB.movies[filmTwo] = gradeTwo;
