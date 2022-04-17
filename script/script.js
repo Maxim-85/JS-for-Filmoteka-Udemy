2bcdf412143930b12f1e423a4730231515c67559
Maxim-85 <k_maks.85@mail.ru>
2022-04-18 00:05:29 +0500

    feat: add conditions and cycles for personalMovieDB.movies

--- script/script.js
+++ script/script.js
@@ -2,18 +2,35 @@
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
+		count: numberOfFilms,
+		movies: {},
+		actors: {},
+		genres: [],
+		privat: false,
 };
 
-const filmOne = prompt('Один из последних просмотренных фильмов', ''),
-      gradeOne = +prompt('Какую оценку поставите?', ''),
 
-      filmTwo = prompt('Один из последних просмотренных фильмов', ''),
-      gradeTwo = +prompt('Какую оценку поставите?', '');
+for (let i = 0; i < 2; i++) {
+	const film = prompt('Один из последних просмотренных фильмов', ''),
+				grade = +prompt('Какую оценку поставите?', '');
+	if (film != '' && grade != '' && film != null && grade != null && film.length < 50) {
+		personalMovieDB.movies[film] = grade;
+		console.log('Done!');
+	} else {
+		console.log('Error');
+		i--;
+	}			
+	// (film != '' && grade != '' && film != null && grade != null && film.length < 50) ? personalMovieDB.movies[film] = grade : i--, console.log('Error');
+}
 
-personalMovieDB.movies[filmOne] = gradeOne;
-personalMovieDB.movies[filmTwo] = gradeTwo;
+if (personalMovieDB.count < 10) {
+	alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+	alert('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+	alert('Вы киноман');
+} else {
+	alert('Произошла ошибка');
+}
+
+console.log(personalMovieDB);
